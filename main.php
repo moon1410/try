e561def2625b820c3354bc8caf5d60e41e2ee84f
moon1410 <gabrielacioaca14@gmail.com>
2016-12-05 18:01:22 +0200

    update main.php

--- main.php
+++ main.php
@@ -5,20 +5,17 @@
    use google\appengine\api\cloud_storage\CloudStorageTools; 
    $user = UserService::getCurrentUser(); 
    if (isset($user)) { 
-   echo sprintf('Bun venit, %s! (<a href="%s">sign out</a>)',$user->getNickname(),UserService::createLogoutUrl('/')); 
+   echo sprintf('Bun venit, %s! (<a href="%s">sign out</a>)',$user->getNickname(),UserService::createLogoutUrl('/'));    
    $mail = $user->getEmail(); 
+   echo sprintf($mail);
    try 
    { 
 
-$to      = 'g.oanaiuliana@gmail.com';
+$to = "gabrielacioaca14@gmail.com";
 $to=  $mail;
 $subject = 'Starea vremii';
 $message = 'Buna ziua, <br /> Puteti vizualiza starea vremii accesand pagina: http://www.accuweather.com/';
-$headers = 'From: webmaster@example.com' . "\r\n" .
-    'Reply-To: webmaster@example.com' . "\r\n" .
-    'X-Mailer: PHP/' . phpversion();
-
-mail($to, $subject, $message, $headers);
+mail($to, $subject, $message);
 }
 catch (InvalidArgumentException $e) { 
    echo "Exception";
