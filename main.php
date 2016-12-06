80e5b232685fd32ef6667e468c181d396ff29606
moon1410 <gabrielacioaca14@gmail.com>
2016-12-06 12:48:35 +0200

    Update main.php

--- main.php
+++ main.php
@@ -1,32 +1,39 @@
-<?php 
-   use google\appengine\api\users\User; 
-   use google\appengine\api\users\UserService; 
-   use \google\appengine\api\mail\Message; 
-   use google\appengine\api\cloud_storage\CloudStorageTools; 
-   $user = UserService::getCurrentUser(); 
-   if (isset($user)) { 
-   echo sprintf('Bun venit, %s! (<a href="%s">sign out</a>)',$user->getNickname(),UserService::createLogoutUrl('/'));    
-   $mail = $user->getEmail(); 
-   echo($mail);
-      try{ 
-         $to = 'gabrielacioaca14@gmail.com';
-         $to .= $mail;
-         $subject = "Starea vremii";
-         $message = "Buna ziua, <br /> Puteti vizualiza starea vremii accesand pagina: http://www.accuweather.com/";
-         
-         if(@mail($to, $subject, $message))
-         {
-          echo("Pe adresa de e-mail ati primit informatii despre starea vremii!");
-         }
-         else{
-            echo "Mail not sent";
-         }
-      
-       catch (InvalidArgumentException $e) { 
-         echo ($e);
-      } 
-   }
-   else { 
-      echo sprintf('<a href="%s">Sign in or register</a>',UserService::createLoginUrl('/')); 
-   } 
-?> 
+<?php
+
+use google\appengine\api\users\User;
+use google\appengine\api\users\UserService;
+use \google\appengine\api\mail\Message;
+use google\appengine\api\cloud_storage\CloudStorageTools;
+
+$user = UserService::getCurrentUser();
+if (isset($user)) {
+  echo sprintf('Bine ai venit in contul tau, %s! (<a href="%s">sign out</a>)',
+               $user->getNickname(),
+		UserService::createLogoutUrl('/'))  ;
+
+echo "<br>", "<br>", "<br>";
+$mail = $user->getEmail();
+
+try
+{$user = UserService::getCurrentUser();
+  $message = new Message();
+  $message->setSender("g.oanaiuliana@gmail.com");
+  $message->addTo("$mail, gitman_oana@yahoo.com");
+  $message->setSubject("Oferta zilei Elefant");
+  $message->setTextBody('Buna ziua, 
+Puteti vizualiza starea vremii accesand pagina: http://www.accuweather.com/');
+   $message->send();
+} catch (InvalidArgumentException $e) {
+  // ...
+}
+
+echo ' Pe adresa de e-mail ai primit informatii despre starea vremii!';
+		
+
+} else {
+  echo sprintf('<a href="%s">Sign in or register</a>',
+               UserService::createLoginUrl('/'));
+		
+}
+
+?>
